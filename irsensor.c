ac54071c6e43f46396c636b3d2862acb2941443c
wainuiwitikapark <wainuiwitikapark@hotmail.co.nz>
2016-05-25 16:32:43 +1200

    comments and slightly edited ir code

--- irsensor.c
+++ irsensor.c
@@ -28,36 +28,36 @@ void check_wall()
 		sensor3_reading = ReadAnalog(2);
 		
 		
-		if (check_front() == 0 && check_left() == 1 && check_right() == 1)
+		if (check_front() == 0 && check_left() == 1 && check_right() == 1) // if walls on left and right, but front is clear
 		{
-		  forwards();
+		  forwards(); // then go straight ahead
 		}
 		
-		else if (check_front() == 1 && check_left() == 1 && check_right() == 1)
+		else if (check_front() == 1 && check_left() == 1 && check_right() == 1) // if stuck
 		{
 		  stop();
-		  turn_around();
+		  turn_around(); // stop and turn around
 		}
 
-		else if (check_left() = 0 && check_right() == 1 && check_front == 1)
+		else if (check_left() = 0 && check_right() == 1 && check_front == 1) // if walls in front and right
 		{
 		  stop();
-		  turn_left();
+		  turn_left(); // then turn left
 		}
 
-		else if (check_right() == 0 && check_left() == 1 && check_front == 1)
+		else if (check_right() == 0 && check_left() == 1 && check_front == 1) // if walls in front and left
 		{
 		  stop();
-		  turn_left();
+		  turn_right(); // turn right
 		}
-		else if (check_front() == 0 && check_left() == 0)
+		else if (check_front() == 0 && check_left() == 0) // if clear ahead and left
 		{
 			stop();
-			turn_left();
+			turn_left(); // choose to turn left
 		}
-		else if (check_front() == 0)
+		else if (check_front() == 0) // if clear ahead
 		{
-			forwards();
+			forwards(); // go straight ahead
 		}
 		
 	}
@@ -65,40 +65,40 @@ void check_wall()
 
 // The below code assums motor_1 is left and motor_2 is right
 
-void stop()
+void stop() // sets both wheels to stop
 {
     set_motor(2, 0);
     set_motor(1, 0);
-	sleep(100;
+	sleep(1000);
 }
 
-void forwards(){
+void forwards(){ // sets both wheels forwards at speed 30
     set_motor(2, default_speed);
     set_motor(1, default_speed);
 }
 
-void turn_around(){
-    set_motor(1, default_speed);
-    set_motor(2, 0);
+void turn_around(){ // sets left wheel at speed 30
+    set_motor(1, default_speed); // wants speed 30 to turn robot around 180 degrees
+    set_motor(2, 0); // sets right wheel to stop
 	sleep(2000);
 }
 
-void turn_left(int factor){
+void turn_right(int factor){ // sets left wheel to speed 30
 	
     int motor_1 = default_speed;
     int motor_2 = 0;
 
-    set_motor(1, motor_1);
-    set_motor(2, motor_2);
+    set_motor(1, motor_1); // wants speed 30 to turn robot right 90 degrees
+    set_motor(2, motor_2); // sets right wheel to stop
 	sleep(1000);
 }
 
-void turn_right(int factor){
+void turn_left(int factor){ // sets right wheel to speed 30
     int motor_1 = 0;
     int motor_2 = default_speed;
 
-    set_motor(1, motor_1);
-    set_motor(2, motor_2);
+    set_motor(1, motor_1); // sets left wheel to stop
+    set_motor(2, motor_2); // wants speed 30 to turn robot left 90 degrees
 	sleep(1000);
 }
 
@@ -157,3 +157,4 @@ int check_right()
 		return 0;
 	}
 }
+
